821b682fc547d3af70b14417f4eb68efcdb6e405
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-08-20 16:51:48 +0200

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Yuri Petrosyan on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
