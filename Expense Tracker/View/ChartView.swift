c7280a08861e1b5a44f81484f537dca7a86b9aec
Yuri Petrosyan <yuriyuri200117@gmail.com>
2024-09-02 19:12:39 +0200

    Add Chart

---
+++ Expense Tracker/View/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  Expense Tracker
+//
+//  Created by Yuri Petrosyan on 9/2/24.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
